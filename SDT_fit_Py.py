2ee75f78fe123f70a5360f03327b14555423e672
trevcaru <97891726+trevcaru@users.noreply.github.com>
2024-03-20 10:21:54 -0400

    Update and rename SDT_fit_helper.py to SDT_fit_Py.py

--- SDT_fit_Py.py
+++ SDT_fit_Py.py
@@ -2,26 +2,22 @@
 # coding: utf-8
 
 # This code is a branch from the original package. Ported to Python by Trevor Caruso. Please contact trevorcaruso@ufl.edu for any questions or comments.
-# 
 # For more information about metacognitive and Type 2 Signal Detection Theory (SDT), visit the following website at http://www.columbia.edu/~bsm2105/type2sdt/ and refer to the following publications:
-# 
+
 # Maniscalco, B., & Lau, H. (2012). "A signal detection theoretic approach for estimating metacognitive sensitivity from confidence ratings." Published in Consciousness and Cognition, 21(1), 422–430. doi:10.1016/j.concog.2011.09.021
-# 
+
 # Maniscalco, B., & Lau, H. (2014). "Signal detection theory analysis of type 1 and type 2 data: meta-d’, response-specific meta-d’, and the unequal variance SDT mode." In S. M. Fleming & C. D. Frith (Eds.), The Cognitive Neuroscience of Metacognition (pp.25-66). Published by Springer.
-# 
+
 # If you utilize these functions, please cite the aforementioned papers and scripts upon which they are based.
-# 
+
 # The same input-format as M & L is expected. As per the original code:
 
-# In[ ]:
-
-
 # INPUTS
-#
+
 # * nR_S1, nR_S2
 # these are vectors containing the total number of responses in
 # each response category, conditional on presentation of S1 and S2.
-#
+
 # e.g. if nR_S1 = [100 50 20 10 5 1], then when stimulus S1 was
 # presented, the subject had the following response counts:
 # responded S1, rating=3 : 100 times
@@ -30,7 +26,7 @@
 # responded S2, rating=1 : 10 times
 # responded S2, rating=2 : 5 times
 # responded S2, rating=3 : 1 time
-#
+
 # The ordering of response / rating counts for S2 should be the same as it
 # is for S1. e.g. if nR_S2 = [3 7 8 12 27 89], then when stimulus S2 was
 # presented, the subject had the following response counts:
@@ -41,11 +37,7 @@
 # responded S2, rating=2 : 27 times
 # responded S2, rating=3 : 89 times
 
-
 # The function's input should consist of counts for each of these responses separately for each stimulus type.
-
-# In[ ]:
-
 
 import numpy as np
 from scipy.optimize import minimize
@@ -144,10 +136,7 @@ def SDT_MLE_fit(nR_S1, nR_S2):
 
     return result_dict
 
-
-# In[20]:
-
-
+#######################
 # Example usage with the provided nR_S1 and nR_S2 (sanity check; result should be ~1)
 nR_S1 = [100, 50, 20, 10, 5, 1]
 nR_S2 = [1, 5, 10, 20, 50, 100]
@@ -155,12 +144,11 @@ nR_S2 = [1, 5, 10, 20, 50, 100]
 result = SDT_MLE_fit(nR_S1, nR_S2)
 print(result)
 
+# the estimated s-value is the ratio of standard deviations for type 1 distributions. Use-case: unequal variance
+# s is the slope of z(HR) vs z(FAR)
+# estimated_s = sd(S1) / sd(S2)
 
 # If using the code to extract the estimated s-value:
-
-# In[21]:
-
-
 # Extracting the s-value from the result and storing it in the variable s_estim
 s_estim = result['s']
 print("s_estim:", s_estim)
